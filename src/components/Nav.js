2b8c2af2aa03008dfea66b62bd64ad105839d831
zettet <tzettersten@gmail.com>
2023-08-28 20:35:56 -0500

    fixing tests

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,12 +4,12 @@ export default class Nav extends React.Component {
     render() {
         return (
             <nav>
-                <ul class="navLinks">
-                    <li class="navLink"><a href="#Home">Home</a></li>
-                    <li class="navLink"><a href="#About">About</a></li>
-                    <li class="navLink"><a href="/Menu">Menu</a></li>
-                    <li class="navLink"><a href="/Reservations">Reservations</a></li>
-                    <li class="navLink"><a href="/Login">Login</a></li>
+                <ul className="navLinks">
+                    <li className="navLink"><a href="#Home">Home</a></li>
+                    <li className="navLink"><a href="#About">About</a></li>
+                    <li className="navLink"><a href="/Menu">Menu</a></li>
+                    <li className="navLink"><a href="/Reservations">Reservations</a></li>
+                    <li className="navLink"><a href="/Login">Login</a></li>
                 </ul>
             </nav>
         )
